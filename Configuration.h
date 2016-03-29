44bd4d8f243dd55473924f6ffcdedbbab6d9b3b1
bracci <manuel.bracher@gmail.com>
2016-03-29 23:00:08 +0200

    tidy

--- Configuration.h
+++ Configuration.h
@@ -67,7 +67,7 @@
 #define LED_DRIVER_LPD8806
 
 #if defined(LED_DRIVER_LPD8806) || defined(LED_DRIVER_NEOPIXEL)
-#define RGB_LEDS
+//#define RGB_LEDS
 #define RGBW_LEDS
 //#define MATRIX_XXL
 #endif
