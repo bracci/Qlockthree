664aedd3009529dc9fb43de8b1aa4d6121192b04
bracci <manuel.bracher@gmail.com>
2016-04-17 00:57:33 +0200

    CLT FW version

--- Qlockthree.ino
+++ Qlockthree.ino
@@ -200,7 +200,7 @@
 #endif
 
 
-#define FIRMWARE_VERSION "V 3.4.9.x"
+#define FIRMWARE_VERSION "CLT OS V2.0, based on Qlockthree by Christian Ashoff"
 
 /*
    Den DEBUG-Schalter gibt es in allen Bibiliotheken. Wird er eingeschaltet, werden ueber den
